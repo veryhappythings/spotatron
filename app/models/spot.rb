566838c2db1335d00d2e42ef2b557cfde0dd5e64
Marcus Cobden <mcobden@maelys.ecs.soton.ac.uk>
2009-02-06 16:55:32 +0000

    Added Spotify uris to the spot model and the main page.

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,4 +2,12 @@ class Spot < ActiveRecord::Base
   belongs_to :user
   
   validates_format_of :url, :with => /\Ahttp:\/\/open\.spotify\.com\/.*/, :message => 'URL is not valid'
+
+  def uri
+    # Naievly convert 1 into 2
+    # 1: http://open.spotify.com/track/5ibnia6j39WwM17OfP7zR1
+    # 2: spotify:track:5ibnia6j39WwM17OfP7zR1
+    
+    self.url.sub('http://open.spotify.com/','spotify:').sub('/',':')
+  end
 end
