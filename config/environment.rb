d945d673d059afebf590db0614352637a9ec673d
Mac <mac@optimorlabs.com>
2009-05-19 17:06:56 +0100

    Added the Acts As Taggable On plugin

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "RedCloth",:lib => "redcloth"
+  config.gem "mbleigh-acts-as-taggable-on", :lib => "acts-as-taggable-on"
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
