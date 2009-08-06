1b2f23cf161ccfa70301dcd4c128d43c9fd6507b
puresock <mac@optimorlabs.com>
2009-08-06 16:27:07 +0100

    Added source to acts-as-taggable-on

--- config/environment.rb
+++ config/environment.rb
@@ -21,15 +21,15 @@ Rails::Initializer.run do |config|
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   config.frameworks -= [:active_resource, :action_mailer]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "RedCloth",:lib => "redcloth"
-  config.gem "mbleigh-acts-as-taggable-on", :lib => "acts-as-taggable-on"
-  
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  config.gem "mbleigh-acts-as-taggable-on", :source => 'http://gems.github.com' :lib => "acts-as-taggable-on"
+
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -48,7 +48,7 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_spotatron_session',
